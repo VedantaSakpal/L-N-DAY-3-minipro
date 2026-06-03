e3ad871377e7d752379652f265b2f763c00a17c5
VedantaSakpal <vsakpal541@gmail.com>
2026-06-03 15:18:03 +0530

    first commit

--- test_app.py
+++ test_app.py
@@ -21,4 +21,7 @@ def test_userDetails():
     response = client.get("/userDetails")
 
     assert response.status_code == 200
+    assert respose.json["name"]=="Vedant"
+    assert respose.json["age"]=="18"
     assert response.json["maritalStatus"] == "Married"
+
